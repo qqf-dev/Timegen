014a3dc4ba2005eb59fa3ac7e65f2a44606e69c8
qqf_dev <qqf1430550377@outlook.com>
2022-04-04 04:06:55 +0800

    init UI

--- mainwindow.py
+++ mainwindow.py
@@ -1,15 +1,17 @@
 # This Python file uses the following encoding: utf-8
 import sys
 from PySide2.QtWidgets import QApplication, QMainWindow
+from PySide2.QtUiTools import QUiLoader
 
 
 class MainWindow(QMainWindow):
     def __init__(self):
+        self.ui = QUiLoader().load('mainwindow.ui')
         QMainWindow.__init__(self)
 
 
 if __name__ == "__main__":
     app = QApplication([])
     window = MainWindow()
-    window.show()
+    window.ui.show()
     sys.exit(app.exec_())
